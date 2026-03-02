3916fba700151985c172cea6d8becca43c95e6bf
Aurora Mędrzycka <aurora@astrigera.dev>
2026-03-02 15:18:19 +0100

    Publish @openstax-poland/eslint-config 0.7.0
    Source commit: 1f43424df7a753cc4172b69d3407c561c9a39fc1

---
+++ base.d.ts
@@ -0,0 +1,5 @@
+import { type Linter } from 'eslint';
+export declare const base: import("eslint/config").Config[];
+export declare const typescript: Linter.Config;
+declare const _default: import("eslint/config").Config[];
+export default _default;
